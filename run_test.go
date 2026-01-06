db7e7b06e873e0accbfb3085ccf5ac5805127fa5
Alexander F. Rødseth <rodseth@gmail.com>
2026-01-06 09:41:05 +0100

    Add functions for running commands

---
+++ run_test.go
@@ -0,0 +1,30 @@
+package files
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSomething(t *testing.T) {
+	if err := Run("ls"); err != nil {
+		t.Error(err)
+	}
+
+	output, err := Shell("echo SHELL")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(output)
+
+	output, err = Bash("echo BASH")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(output)
+
+	output, err = Fish("echo FISH")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(output)
+}
